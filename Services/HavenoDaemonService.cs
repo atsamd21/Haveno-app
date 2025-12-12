358562f917251f69459a104a87108fe3cd706fae
atsamd21 <atsamd21@proton.me>
2025-12-12 16:37:17 +0100

    Move IHavenoDaemonService interface to separate file

--- Services/HavenoDaemonService.cs
+++ Services/HavenoDaemonService.cs
@@ -6,20 +6,6 @@ using System.Runtime.InteropServices;
 
 namespace Manta.Services;
 
-public interface IHavenoDaemonService
-{
-    Task<(bool, string)> GetIsDaemonInstalledAsync();
-    Task InstallHavenoDaemonAsync(IProgress<double> progressCb);
-    Task<bool> TryStartLocalHavenoDaemonAsync(string password, string host, Action<string>? progressCb = default);
-    Task<bool> TryStartTorAsync();
-    Task<bool> WaitHavenoDaemonInitializedAsync(CancellationToken cancellationToken = default);
-    Task<bool> WaitWalletInitializedAsync(CancellationToken cancellationToken = default);
-    Task<bool> IsHavenoDaemonRunningAsync(CancellationToken cancellationToken = default);
-    Task StopHavenoDaemonAsync();
-    Task TryUpdateHavenoAsync(IProgress<double> progressCb);
-    string GetDaemonPath();
-}
-
 public abstract class HavenoDaemonServiceBase : IHavenoDaemonService
 {
     private readonly IHavenoVersionService _versionService;
---
+++ Services/IHavenoDaemonService.cs
@@ -0,0 +1,15 @@
+﻿namespace Manta.Services;
+
+public interface IHavenoDaemonService
+{
+    Task<(bool, string)> GetIsDaemonInstalledAsync();
+    Task InstallHavenoDaemonAsync(IProgress<double> progressCb);
+    Task<bool> TryStartLocalHavenoDaemonAsync(string password, string host, Action<string>? progressCb = default);
+    Task<bool> TryStartTorAsync();
+    Task<bool> WaitHavenoDaemonInitializedAsync(CancellationToken cancellationToken = default);
+    Task<bool> WaitWalletInitializedAsync(CancellationToken cancellationToken = default);
+    Task<bool> IsHavenoDaemonRunningAsync(CancellationToken cancellationToken = default);
+    Task StopHavenoDaemonAsync();
+    Task TryUpdateHavenoAsync(IProgress<double> progressCb);
+    string GetDaemonPath();
+}
